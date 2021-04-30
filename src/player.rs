42c439432fb7471e45f9a1fb8f02da35b3ff8f76
IfitNaoned <haku_cs@hotmail.com>
2021-04-30 20:49:56 +0200

    dev

--- src/player.rs
+++ src/player.rs
@@ -5,9 +5,14 @@ fn spawn_player() {
     println!("spawn_player!");
 }
 
+fn game() {
+    //println!("game!");
+}
+
 pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_system_set(SystemSet::on_enter(AppState::Game).with_system(spawn_player.system()));
+        app.add_system_set(SystemSet::on_enter(AppState::Game).with_system(spawn_player.system()))
+            .add_system_set(SystemSet::on_update(AppState::Game).with_system(game.system()));
     }
 }
